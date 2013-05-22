2cbd6ec636095c875e2984923d463c029082cf02
Maher El-Atawy <maher.atawy@gmail.com>
2013-05-22 17:52:06 +0200

    Initial commit

---
+++ avogadros_project/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AvogadrosProject::Application.config.secret_token = 'cc27902ffdd43b7ba0cd75a2e88ecb75fa4e9166f6bfceac1b4328414b17184887eaef397fbf648d10cbe28eeffaabd8c26058447749e766e45aa05cdc8d242b'
